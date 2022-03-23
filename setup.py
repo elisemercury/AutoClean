5214940a08e3dc8febca87ca42d6dc8c386cbcad
elisemercury <elisejlandman@hotmail.com>
2022-03-23 12:06:17 +0100

    Various updates

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(base_dir, "README.md")) as f:
 setup(
   name = 'AutoClean',         
   packages = ['AutoClean'],   
-  version = 'beta1',      
+  version = 'v0.0.1-alpha',      
   license='MIT',        
   description = 'AutoClean - Python Package for Automated Preprocessing of Datasets.', 
   long_description=long_description,
@@ -17,16 +17,17 @@ setup(
   author = 'Elise Landman',                  
   author_email = 'elisejlandman@hotmail.com', 
   url = 'https://github.com/elisemercury/AutoClean', 
-  download_url = 'https://github.com/elisemercury/Duplicate-Image-Finder/archive/refs/tags/v2.2.tar.gz',    # change everytime for each new release
+  download_url = 'https://github.com/elisemercury/AutoClean/archive/refs/tags/0.0.1-alpha.tar.gz',    # change everytime for each new release
   keywords = ['automated', 'cleaning', 'preprocessing', "autoclean"],  
   install_requires=[          
-          'scikit-image',
-          'matplotlib',
+          'scikit-learn',
+          'timeit',
           'numpy',
-          'opencv-python',
+          'pandas',
+          'loguru'
       ],
   classifiers=[
-    'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',    
